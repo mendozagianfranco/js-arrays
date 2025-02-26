1e77541292c80028760de7fdd315b39d7a5860bf
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-26 13:24:28 +0100

    feat: complete 3 task arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -20,7 +20,9 @@ console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
