e7871c61690e99a0b4357f2f4d0a0c5db2bb918e
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-26 13:24:54 +0100

    feat: complete 4 task arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,7 +25,9 @@ console.log(lastTeacher);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
